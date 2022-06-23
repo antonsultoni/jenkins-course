cbd8bb298eecdc79b2689e43dce9eb4e71954c76
antonsultoni <107566856+antonsultoni@users.noreply.github.com>
2022-06-23 17:32:21 +0700

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/antonsultoni/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('antonsultoni')
-            node / gitConfigEmail('antnsltn@gmail.com')
+        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
     triggers {
